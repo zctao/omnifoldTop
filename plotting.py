0b26d6573e1d265835bdd976f1891313101cddd0
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2020-07-09 16:40:47 -0700

    Deal with zero bins better when dividing two histograms

--- plotting.py
+++ plotting.py
@@ -19,18 +19,19 @@ def plot_ratios(ax, midbins, binwidth, hist_truth, hists_unfolded, colors, hist_
     ax.plot([np.min(midbins), np.max(midbins)], [1, 1], '-', color=truth_style['edgecolor'], lw=0.75)
     
     if hist_truth_unc is not None:
-        truth_unc_ratio = hist_truth_unc/(hist_truth + 10**-50)
+        truth_unc_ratio = np.divide(hist_truth_unc, hist_truth, out=np.zeros_like(hist_truth), where=(hist_truth!=0))
         ax.fill_between(midbins, 1-truth_unc_ratio, 1+truth_unc_ratio, facecolor=truth_style['facecolor'], zorder=-2)
         
     for i, hist_uf in enumerate(hists_unfolded):
         if hist_uf is None:
             continue
-        ratio = hist_uf/(hist_truth + 10**-50)
+        ymin, ymax = ax.get_ylim()
+        ratio = np.divide(hist_uf, hist_truth, out=np.ones_like(hist_truth)*ymin, where=(hist_truth!=0))
 
         ratio_unc = None
         if hists_unfolded_unc is not None:
             if hists_unfolded_unc[i] is not None:
-                ratio_unc = hists_unfolded_unc[i]/(hist_truth + 10**-50)
+                ratio_unc = np.divide(hists_unfolded_unc[i], hist_truth, out=np.zeros_like(hist_truth), where=(hist_truth!=0))
 
         ax.errorbar(midbins, ratio, xerr=binwidth/2, yerr=ratio_unc, color=colors[i], **modplot.style('errorbar'))
 
