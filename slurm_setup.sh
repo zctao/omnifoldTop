c0459bde0472f2c282e8c7543ba410f6092b7e81
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2020-07-10 17:06:02 -0700

    update setup script

--- slurm_setup.sh
+++ slurm_setup.sh
@@ -10,6 +10,7 @@ source $SLURM_TMPDIR/venv/bin/activate
 pip install --no-index --upgrade pip
 # Install pre-downloaded packages
 pip install ~/work/wheels/EnergyFlow-1.0.3-py2.py3-none-any.whl
+pip install ~/work/wheels/sklearn-0.0.tar.gz
 pip install ~/work/wheels/PyPDF2-1.26.0.tar.gz
 # Install the rest of packages
 pip install --no-index -r $SOURCEDIR/requirements_cedar_gpu.txt
