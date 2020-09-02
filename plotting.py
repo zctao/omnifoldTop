e25a7857e527c4807dd95d84f354e3c0e3e183d4
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2020-09-02 10:49:50 -0700

    Add methods to plot histograms and graphs

--- plotting.py
+++ plotting.py
@@ -10,6 +10,8 @@ from util import add_histograms
 
 # plotting styles
 hist_style = {'histtype': 'step', 'density': False, 'lw': 1, 'zorder': 2}
+graph_style = {'fmt': 'o', 'lw': 1.5, 'capsize': 1.5, 'capthick': 1, 'markersize': 1.5}
+leg_style = {'handlelength': 2.0, 'loc': 'best', 'frameon': False, 'numpoints': 1, 'fontsize': 'small'}
 
 data_style = {'color': 'black', 'label':  'Data', **hist_style}
 sim_style = {'color': 'orange', 'label':  'Sim.', **hist_style}
@@ -23,8 +25,22 @@ ibu_style = {'ls': '-', 'marker': 'o', 'ms': 2.5, 'color': 'gray', 'zorder': 1, 
 
 omnifold_style = {'ls': '-', 'marker': 's', 'ms': 2.5, 'color': 'tab:red', 'zorder': 3, 'label':'MultiFold'}
 
+def init_fig(title='', xlabel='', ylabel=''):
+    fig, ax = plt.subplots()
 
-def plot_ratios(ax, bins, hist_denom, hists_numer, hist_denom_unc=None, hists_numer_unc=None, color_denom_line='tomato', color_denom_fill='silver', colors_numer=None):
+    if title:
+        ax.set_title(title)
+    if xlabel:
+        ax.set_xlabel(xlabel)
+    if ylabel:
+        ax.set_ylabel(ylabel)
+
+    return fig, ax
+
+def set_default_colors(ncolors):
+    return plt.rcParams['axes.prop_cycle'].by_key()['color'][:ncolors]
+
+def draw_ratios(ax, bins, hist_denom, hists_numer, hist_denom_unc=None, hists_numer_unc=None, color_denom_line='tomato', color_denom_fill='silver', colors_numer=None):
     midbins = (bins[:-1] + bins[1:]) / 2
     binwidth = bins[1] - bins[0]
 
@@ -38,7 +54,7 @@ def plot_ratios(ax, bins, hist_denom, hists_numer, hist_denom_unc=None, hists_nu
     if colors_numer is not None:
         assert(len(colors_numer)==len(hists_numer))
     else:
-        colors_numer = plt.rcParams['axes.prop_cycle'].by_key()['color'][:len(hists_numer)]
+        colors_numer = set_default_colors(len(hists_numer))
 
     for i, hist_num in enumerate(hists_numer):
         if hist_num is None:
@@ -54,13 +70,13 @@ def plot_ratios(ax, bins, hist_denom, hists_numer, hist_denom_unc=None, hists_nu
 
         ax.errorbar(midbins, ratio, xerr=binwidth/2, yerr=ratio_unc, color=colors_numer[i], **modplot.style('errorbar'))
 
-def plot_legend(ax, **config):
+def draw_legend(ax, **config):
     loc = config.get('legend_loc', 'best')
     ncol = config.get('legend_ncol', 2)
     #order = [3, 4, 2, 5, 0, 1] if ncol==2 else [3, 5, 4, 0, 2, 1]
     modplot.legend(ax=ax, loc=loc, ncol=ncol, frameon=False, fontsize='x-small')
 
-def plot_stamp(ax, texts, x=0.5, y=0.5, dy=0.045):
+def draw_stamp(ax, texts, x=0.5, y=0.5, dy=0.045):
     textopts = {'horizontalalignment': 'left',
                 'verticalalignment': 'center',
                 'fontsize': 5.,
@@ -70,18 +86,18 @@ def plot_stamp(ax, texts, x=0.5, y=0.5, dy=0.045):
         if txt is not None:
             ax.text(x, y-i*dy, txt, **textopts)
 
-def plot_histogram(ax, bin_edges, hist, hist_unc=None, **styles):
+def draw_histogram(ax, bin_edges, hist, hist_unc=None, **styles):
     midbins = (bin_edges[:-1] + bin_edges[1:]) / 2
 
     ax.hist(midbins, bin_edges, weights=hist, **styles)
     # TODO: uncertainty hist_unc
 
-def plot_stacked_histograms(ax, bin_edges, hists, hists_unc=None, labels=None,
+def draw_stacked_histograms(ax, bin_edges, hists, hists_unc=None, labels=None,
                             colors=None):
     midbins = (bin_edges[:-1] + bin_edges[1:]) / 2
 
     if colors is None:
-        colors = plt.rcParams['axes.prop_cycle'].by_key()['color'][:len(hists)]
+        colors = set_default_colors(len(hists))
     assert(len(colors)==len(hists))
 
     if labels is None:
@@ -94,17 +110,80 @@ def plot_stacked_histograms(ax, bin_edges, hists, hists_unc=None, labels=None,
             stacked = True, histtype='step', fill=True)
     # TODO: uncertainty
 
-def plot_hist_as_graph(ax, bin_edges, hist, hist_unc=None, **styles):
-    midbins = (bin_edges[:-1] + bin_edges[1:]) / 2
-
-    ax.plot(midbins, hist, **styles)
-    # TODO: uncertainty hist_unc
-
-def plot_hist_fill(ax, bin_edges, hist, hist_unc=None, **styles):
+def draw_hist_fill(ax, bin_edges, hist, hist_unc=None, **styles):
     midbins = (bin_edges[:-1] + bin_edges[1:]) / 2
 
     ax.fill_between(midbins, hist, **styles)
     # TODO: uncertainty?
+
+def draw_hist_as_graph(ax, bin_edges, hist, hist_unc=None, **styles):
+    midbins = (bin_edges[:-1] + bin_edges[1:]) / 2
+    binwidth = bin_edges[1] - bin_edges[0]
+
+    yerr = hist_unc
+    xerr = None if yerr is None else binwidth/2
+
+    ax.errorbar(midbins, hist, xerr=xerr, yerr=yerr, **styles)
+
+def plot_graphs(figname, data_arrays, labels=None, title='', xlabel='', ylabel='', colors=None, markers=None):
+    fig, ax = init_fig(title, xlabel, ylabel)
+
+    if colors is None:
+        colors = set_default_colors(len(data_arrays))
+    else:
+        assert(len(data_arrays)==len(colors))
+
+    # TODO: error
+    for i, (x, y) in enumerate(data_arrays):
+        label = None if labels is None else labels[i]
+        marker = None if markers is None else markers[i]
+        ax.errorbar(x, y, label=label, marker=marker, color=colors[i], **graph_style)
+
+    # plot legend if needed
+    if labels is not None:
+        ax.legend(**leg_style)
+
+    fig.savefig(figname)
+    plt.close(fig)
+
+def plot_histograms1d(figname, bins, hists, hists_err=None, labels=None, title="", xlabel="", ylabel="", colors=None, plottypes=None, marker='o'):
+    fig, ax = init_fig(title, xlabel, ylabel)
+
+    if colors is None:
+        colors = set_default_colors(len(hists))
+    else:
+        assert(len(colors)==len(hists))
+
+    if labels is None:
+        labels = [""]*len(hists)
+    else:
+        assert(len(labels)==len(hists))
+
+    if hists_err is not None:
+        assert(len(hists_err)==len(hists))
+    else:
+        hists_err = [np.zeros_like(h) for h in hists]
+
+    if plottypes is None:
+        plottypes = ['h']*len(hists)
+    else:
+        assert(len(plottypes)==len(hists))
+
+    for i, h in enumerate(hists):
+        label = None if labels is None else labels[i]
+        if plottypes[i] == "g":
+            draw_hist_as_graph(ax, bins, h, hists_err[i], label=label, color=colors[i], marker=marker, **graph_style)
+        elif plottypes[i] == "h":
+            draw_histogram(ax, bins, h, hists_err[i], label=label, color=colors[i], fill=False, **hist_style)
+        else:
+            raise RuntimeError("Unknown plot type {}".format(plottypes[i]))
+
+    # plot legend if needed
+    if labels is not None:
+        ax.legend(**leg_style)
+
+    fig.savefig(figname)
+    plt.close(fig)
 
 def plot_reco_variable(bins, histogram_obs, histogram_sig,
                         histogram_bkg=(None,None),
@@ -145,15 +224,15 @@ def plot_reco_variable(bins, histogram_obs, histogram_sig,
         labels = [bkg_style['label'], sim_style['label']]
         colors = [bkg_style['color'], sim_style['color']]
 
-    plot_stacked_histograms(ax0, bins, hists_stack, labels=labels, colors=colors)
-    plot_histogram(ax0, bins, hist_obs, **data_style)
+    draw_stacked_histograms(ax0, bins, hists_stack, labels=labels, colors=colors)
+    draw_histogram(ax0, bins, hist_obs, **data_style)
 
     # data/mc ratio
     hist_mc, hist_mc_unc = add_histograms(hist_sig, hist_bkg, hist_sig_unc, hist_bkg_unc)
-    plot_ratios(ax1, bins, hist_mc, [hist_obs], hist_mc_unc, [hist_obs_unc], colors_numer=[data_style['color']])
+    draw_ratios(ax1, bins, hist_mc, [hist_obs], hist_mc_unc, [hist_obs_unc], colors_numer=[data_style['color']])
 
     # legend
-    plot_legend(ax0, **config)
+    draw_legend(ax0, **config)
 
     # save plot
     fig.savefig(figname, bbox_inches='tight')
@@ -179,50 +258,51 @@ def plot_results(bins_gen, histogram_gen, histogram_of, histogram_ibu=(None,None
     # signal prior
     hist_gen, hist_gen_unc = histogram_gen
     ymax = max(hist_gen.max(), ymax)
-    plot_hist_as_graph(ax0, bins_gen, hist_gen, **gen_style)
+    draw_hist_as_graph(ax0, bins_gen, hist_gen, **gen_style)
 
     # if truth is known
     hist_truth, hist_truth_unc = histogram_truth
     if hist_truth is not None:
         ymax = max(hist_truth.max(), ymax)
-        plot_hist_fill(ax0, bins_gen, hist_truth, **truth_style)
+        draw_hist_fill(ax0, bins_gen, hist_truth, **truth_style)
 
     # unfolded distributions
     # omnifold
     hist_of, hist_of_unc = histogram_of
     ymax = max(hist_of.max(), ymax)
-    plot_hist_as_graph(ax0, bins_gen, hist_of, **omnifold_style)
+    draw_hist_as_graph(ax0, bins_gen, hist_of, **omnifold_style)
 
     # iterative Bayesian unfolding
     hist_ibu, hist_ibu_unc = histogram_ibu
     if hist_ibu is not None:
         ymax = max(hist_ibu.max(), ymax)
-        plot_hist_as_graph(ax0, bins_gen, hist_ibu, **ibu_style)
+        draw_hist_as_graph(ax0, bins_gen, hist_ibu, **ibu_style)
 
     # update y-axis limit
     ax0.set_ylim((0, ymax*1.2))
 
     if ax1:
         #  ratios of the unfolded distributions to truth
-        plot_ratios(ax1, bins_gen, hist_truth, [hist_ibu, hist_of],
+        draw_ratios(ax1, bins_gen, hist_truth, [hist_ibu, hist_of],
                     hist_truth_unc, [hist_ibu_unc, hist_of_unc],
                     truth_style['edgecolor'], truth_style['facecolor'],
                     [ibu_style['color'], omnifold_style['color']])
 
-    plot_legend(ax0, **config)
+    draw_legend(ax0, **config)
 
-    plot_stamp(ax0, texts, config['stamp_xy'][0], config['stamp_xy'][1])
+    draw_stamp(ax0, texts, config['stamp_xy'][0], config['stamp_xy'][1])
 
     # save plot
     fig.savefig(figname, bbox_inches='tight')
 
     plt.close(fig)
 
-def plot_histogram2d(figname, h2d, xedges, yedges, variable):
-    fig, ax = plt.subplots()
-    ax.set_title('Detector Response')
-    ax.set_xlabel('Detector-level {}'.format(variable))
-    ax.set_ylabel('Truth-level {}'.format(variable))
+def plot_response(figname, h2d, xedges, yedges, variable):
+    fig, ax = init_fig(
+        title='Detector Response',
+        xlabel='Detector-level {}'.format(variable),
+        ylabel='Truth-level {}'.format(variable)
+    )
     X, Y = np.meshgrid(xedges, yedges)
     im = ax.pcolormesh(X, Y, h2d.T*100, cmap='Greens')
     fig.colorbar(im, ax=ax, label="%")
