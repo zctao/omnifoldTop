14737c583d567c1f80cdc748f9d232b465ebcfe4
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2022-03-29 23:40:03 -0700

    Move unfold to scripts

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ VENV_DIR=${1:-venv}
 export SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # cf. https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 
-source $SOURCE_DIR/scripts/setup_venv.sh $VENV_DIR $SOURCE_DIR/requirements.txt
+source ${SOURCE_DIR}/scripts/setup_venv.sh $VENV_DIR ${SOURCE_DIR}/requirements.txt
 
-export PYTHONPATH=$SOURCE_DIR/python:$SOURCE_DIR:$PYTHONPATH
+export PYTHONPATH=${SOURCE_DIR}/python:${SOURCE_DIR}/scripts:${SOURCE_DIR}:$PYTHONPATH
