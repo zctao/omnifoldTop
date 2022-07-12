fb8c7d07670c1e0b0497c34b124484669159456c
theo1701 <theo1701@student.ubc.ca>
2022-07-11 17:22:29 -0700

    nominal and resample functions

--- test/tests_plotter.py
+++ test/tests_plotter.py
@@ -21,7 +21,7 @@ styles = [
 
 def read_metric(observable, result_path):
     """
-    read and return the metric for each observable in result_path
+    read and return the metric for the observable in result_path
 
     arguments
     ---------
@@ -40,3 +40,58 @@ def read_metric(observable, result_path):
     with open(path, "r") as file:
         metric = json.load(file)
     return metric
+
+def nominal(observable, result_path):
+    """
+    read and return the nominal part of the metric for the observable in result_path
+
+    arguments
+    ---------
+    observable: str
+        the observable name
+    result_path: str
+        path to the result folder
+
+    returns
+    -------
+    nominal: dict
+        dictionary containing the content of the metric file that corresponds to nominal
+    """
+    metric = read_metric(observable, result_path)
+    return metric["nominal"]
+
+def resample(observable, result_path):
+    """
+    read and return the resample part of the metric for the observable in result_path
+
+    arguments
+    ---------
+    observable: str
+        the observable name
+    result_path: str
+        path to the result folder
+
+    returns
+    -------
+    resample: dict
+        dictionary containing the content of the metric file that corresponds to nominal
+    """
+    metric = read_metric(observable, result_path)
+    return metric["resample"]
+
+def chi2(metric):
+    """
+    extract the chi2/ndf values from existing dictionary that represents either the "nominal" or "resample" part of the metric
+
+    arguments
+    ---------
+    metric: dict
+        a dictionary representing either the "nominal" or "resample" part of the metric
+
+    returns
+    -------
+    chi2: numpy array
+        a 1d array consisting of the chi2/ndf values for each iteration if metric is "nominal"
+        a 2d array of shape (nruns, n iterations) of chi2/ndf values if metric is "resample"
+    """
+    return metric["Chi2"]["chi2/ndf"]
