ee77366ce77ef408831fc4e00ccfc80de1ff10b5
Theodore Yu <theo1701@student.ubc.ca>
2022-07-20 16:19:47 -0700

    class documentation for new callback

--- python/callbacks.py
+++ python/callbacks.py
@@ -14,6 +14,13 @@ class PrintLearningRate(keras.callbacks.Callback):
         logger.debug("Current learning rate is {0}".format(learning_rate))
 
 class EarlyLocking(keras.callbacks.Callback):
+    """
+    Monitors the selected performance parameter for all the parallel models. Once the epoch without improvement for a parallel model exceeds
+    set patience, it is locked out from training with trainable set to false. Its weights will be restored to that of patience epoch ago (best result).
+    The entire training process will be terminated when every parallel model has stopped improving.
+    """
+    def __init__(self, )
+
     def on_epoch_end(self, epoch, logs=None):
         for key, value in self.model._get_trainable_state().items():
             print(key, value)
