7f49c6fc3896ad3c254bf8688632adf045f882c5
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2022-11-02 21:13:37 -0700

    Add an option to generate 2D toy data

--- python/datahandler.py
+++ python/datahandler.py
@@ -570,24 +570,8 @@ class DataToy(DataHandler):
     The truth distribution is sampled from a normal distribution specified
     by `mu` and `sigma`. The reconstructed distribution is obtained by adding
     Gaussian noise with standard deviation 1 to the truth distribution.
-
-    Parameters
-    ----------
-    nevents : positive int
-        Number of events in the dataset
-    mu : float, default: 0
-        Mean of the truth distribution.
-    sigma : positive float, default: 1
-        Standard deviation of the truth distribution.
-    epsilon : positive float, default: 1
-        Standard deviation for detector smearing
-    eff : positive float, default: 1
-        Reconstruction efficiency. Fraction of events in truth that are also in reco
-    acc : positivee float, default: 1
-        Acceptance. Fraction of the events in reco that also in truth
-    dummy_value : float, default -10.
-        Value assigned to events that are in reco but not truth or in truth but not reco
     """
+
     def __init__(self):
         self.data_reco = None
         self.data_truth = None
@@ -599,37 +583,86 @@ class DataToy(DataHandler):
     def generate(
         self,
         nevents,
+        varnames = ['x'],
         mu = 0.,
         sigma = 1.,
-        epsilon = 1.,
+        covariance = 1.,
         eff = 1.,
         acc = 1.,
         dummy_value = -10.
         ):
+        """
+        Parameters
+        ----------
+        nevents : positive int
+            Number of events in the dataset
+        varname :  list of str, default: ['x']
+            List of variable names
+        mu : float or sequence of float, default: 0
+            Mean of the truth distribution.
+        sigma : positive float or list of float, default: 1
+            Standard deviation of the truth distribution.
+        covariance : float or 1D array of float or 2D array of float, default: 1
+            Covariance for detector smearing
+        eff : positive float, default: 1
+            Reconstruction efficiency. Fraction of events in truth that are also in reco
+        acc : positivee float, default: 1
+            Acceptance. Fraction of the events in reco that also in truth
+        dummy_value : float, default -10.
+            Value assigned to events that are in reco but not truth or in truth but not reco
+        """
+
+        if np.asarray(mu).ndim == 0: # a float
+            mu_arr = np.asarray([mu]) # change to a 1D array
+        else:
+            mu_arr = np.asarray(mu)
+
+        if np.asarray(sigma).ndim == 0: # a float
+            sigma_arr = np.asarray([sigma]) # change to a 1D array
+        else:
+            sigma_arr = np.asarray(sigma)
+
+        if np.asarray(covariance).ndim == 0:
+            cov = np.diag([covariance]*len(varnames))
+        elif np.asarray(covariance).ndim == 1:
+            cov = np.diag(covariance)
+        elif np.asarray(covariance).ndim == 2:
+            cov = covariance
+        else:
+            print(f"ERROR: covariance has to be a float, 1D array of float, or 2D array of float")
+            return
+
         # generate toy data
         # truth level
-        array_truth = np.random.normal(mu, sigma, nevents)
+        dtype_truth = [(v+'_truth', 'float') for v in varnames]
+        self.data_truth = np.full(nevents, dummy_value, dtype=dtype_truth)
+        for m, s, v in zip(mu_arr, sigma_arr , varnames):
+            self.data_truth[v+'_truth'] = np.random.normal(m, s, nevents)
 
-        # detector smearing
-        array_reco = np.array([(x + np.random.normal(0, epsilon)) for x in array_truth])
+        # detector level
+        # define detector smearing
+        def measure(*data):
+            d = np.asarray(data)
+            s = np.random.multivariate_normal([0.]*len(data), cov=cov)
+            return tuple(d+s)
+
+        #after smearing
+        dtype_reco = [(v+'_reco', 'float') for v in varnames]
+        self.data_reco = np.array([measure(*data) for data in self.data_truth], dtype=dtype_reco)
 
         # efficiency
         if eff < 1:
             self.pass_reco = np.random.binomial(1, eff, nevents).astype(bool)
-            array_reco[~self.pass_reco] = dummy_value
+            self.data_reco[~self.pass_reco] = dummy_value
         else:
             self.pass_reco = np.full(nevents, True)
 
         # acceptance
         if acc < 1:
             self.pass_truth = np.random.binomial(1, acc, nevents).astype(bool)
-            array_truth[~self.pass_truth] = dummy_value
+            self.data_truth[~self.pass_truth] = dummy_value
         else:
             self.pass_truth = np.full(nevents, True)
-
-        # convert to recarray
-        self.data_truth = np.array(array_truth, dtype=[('x_truth', 'float')])
-        self.data_reco = np.array(array_reco, dtype=[('x_reco','float')])
 
         # all event weights are one for now
         self.weights = np.ones(nevents)
--- scripts/generate_toydata.py
+++ scripts/generate_toydata.py
@@ -6,22 +6,21 @@ def generate_toydata(
     nevents,
     filepath_sim,
     filepath_obs,
-    mu_sim = 0,
-    sigma_sim = 1,
-    mu_obs = 0.2,
-    sigma_obs = 0.8,
-    eff=1,
-    acc=1
-):
-
-    # Smaring
-    epsilon = sigma_sim
+    varnames,
+    mu_sim,
+    sigma_sim,
+    mu_obs,
+    sigma_obs,
+    cov_meas,
+    eff,
+    acc
+    ):
 
     # Gen and Sim
     toy_sim = DataToy()
-    print(f"Generating toy data sim: mu={mu_sim}, sigma={sigma_sim}, epsilon={epsilon}, eff={eff}, acc={acc}")
+    print(f"Generating toy data sim: mu={mu_sim}, sigma={sigma_sim}, cov_meas={cov_meas}, eff={eff}, acc={acc}")
     t_sim_start = time.time()
-    toy_sim.generate(nevents=nevents, mu=mu_sim, sigma=sigma_sim, epsilon=epsilon, eff=eff, acc=acc)
+    toy_sim.generate(nevents=nevents, varnames=varnames, mu=mu_sim, sigma=sigma_sim, covariance=cov_meas, eff=eff, acc=acc)
     t_sim_done = time.time()
     print(f"Done. {(t_sim_done-t_sim_start):.3f} seconds")
 
@@ -30,9 +29,9 @@ def generate_toydata(
 
     # Data and Truth
     toy_obs = DataToy()
-    print(f"Generating toy data obs: mu={mu_obs}, sigma={sigma_obs}, epsilon={epsilon}, eff={eff}, acc={acc}")
+    print(f"Generating toy data obs: mu={mu_obs}, sigma={sigma_obs}, cov_meas={cov_meas}, eff={eff}, acc={acc}")
     t_obs_start = time.time()
-    toy_obs.generate(nevents=nevents, mu=mu_obs, sigma=sigma_obs, epsilon=epsilon, eff=eff, acc=acc)
+    toy_obs.generate(nevents=nevents, varnames=varnames, mu=mu_obs, sigma=sigma_obs, covariance=cov_meas, eff=eff, acc=acc)
     t_obs_done = time.time()
     print(f"Done. {(t_obs_done-t_obs_start):.3f} seconds")
 
@@ -55,7 +54,49 @@ if __name__ == "__main__":
                         help="Efficiency")
     parser.add_argument('-a', '--acc', type=float, default=1., 
                         help="Acceptance")
+    parser.add_argument('-p', '--preset', choices=['1d', '2d_ind', '2d_cor'], default='1d')
 
     args = parser.parse_args()
 
-    generate_toydata(**vars(args))
+    if args.preset == '1d':
+        generate_toydata(
+            nevents = args.nevents,
+            filepath_sim = args.filepath_sim,
+            filepath_obs = args.filepath_obs,
+            eff = args.eff,
+            acc = args.acc,
+            varnames = ['x'],
+            mu_sim = 0.,
+            sigma_sim = 1.,
+            mu_obs = 0.2,
+            sigma_obs = 0.8,
+            cov_meas = 1.
+        )
+    elif args.preset == '2d_ind':
+        generate_toydata(
+            nevents = args.nevents,
+            filepath_sim = args.filepath_sim,
+            filepath_obs = args.filepath_obs,
+            eff = args.eff,
+            acc = args.acc,
+            varnames = ['x', 'y'],
+            mu_sim = [0., 0.],
+            sigma_sim = [1., 1.],
+            mu_obs = [0.2, -0.3],
+            sigma_obs = [0.8, 0.9],
+            cov_meas = [[1.,0.],[0.,1.]]
+        )
+    elif args.preset == '2d_cor':
+        generate_toydata(
+            nevents = args.nevents,
+            filepath_sim = args.filepath_sim,
+            filepath_obs = args.filepath_obs,
+            eff = args.eff,
+            acc = args.acc,
+            varnames = ['x', 'y'],
+            mu_sim = [0., 0.],
+            sigma_sim = [1., 1.],
+            mu_obs = [0.2, -0.3],
+            sigma_obs = [0.8, 0.9],
+            cov_meas = [[1.,-0.5],[-0.5,1.]]
+        )
