373f9447fe07c7fd78c3d9b22f9e71a110688bf5
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2023-01-02 23:30:57 -0800

    Move scripts/createRun2Config.py to scripts/ttbarDiffXsRun2/createRun2Config.py

--- scripts/ttbarDiffXsRun2/make_run2_config.sh
+++ scripts/ttbarDiffXsRun2/make_run2_config.sh
@@ -4,7 +4,7 @@ result_top_dir=${1:-${HOME}/data/OmniFoldOutputs/Run2}
 samples_dir=${2:-${HOME}/atlasserv/NtupleTT/latest}
 
 for ch in ljets; do  # or: ejets mjets
-    python scripts/createRun2Config.py \
+    python scripts/ttbarDiffXsRun2createRun2Config.py \
            -d ${samples_dir} \
            -c ${ch} \
            -r ${result_top_dir} \
