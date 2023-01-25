02aa0b6768c69378ac4e6a5fa05abfe88e6cbecd
Theodore Yu <theo1701@student.ubc.ca>
2023-01-24 22:56:11 -0800

    Routing model training to custom tracker model

--- python/customModel.py
+++ python/customModel.py
@@ -3,4 +3,8 @@ from tensorflow import keras
 
 class LossTrackerModel(keras.models.Model):
 	def train_step(self, data):
+		"""
+		Overriding the training step from parent keras.models.Model gives a window during each traning step to take a look
+		at the input data and the output loss, which can be passed to the tracker model for analysis.
+		"""
 		return super().train_step(data)
