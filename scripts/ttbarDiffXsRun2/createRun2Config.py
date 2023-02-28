5923fe2b4c130a2fc1b70a33632a849e23f9d70f
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2023-02-27 17:20:53 -0800

    Refactor createRun2Config.py

--- scripts/ttbarDiffXsRun2/createRun2Config.py
+++ scripts/ttbarDiffXsRun2/createRun2Config.py
@@ -7,6 +7,258 @@ import util
 from ttbarDiffXsRun2.systematics import get_systematics
 #from ttbarDiffXsRun2.systematics import syst_dict, select_systematics
 
+def subCampaigns_to_years(subcampaigns):
+    years = []
+    for e in subcampaigns:
+        if e == "mc16a":
+            years += [2015, 2016]
+        elif e == "mc16d":
+            years += [2017]
+        elif e == "mc16e":
+            years += [2018]
+        else:
+            raise RuntimeError(f"Unknown MC subcampaign {e}")
+
+    return years
+
+def get_samples_data(
+    sample_dir, # top direcotry to look for sample files
+    category = 'ljets', # "ejets" or "mjets" or "ljets"
+    subcampaigns = ["mc16a", "mc16d", "mc16e"],
+    check_exist = True, # If True, check if the files exist
+    ):
+
+    years = subCampaigns_to_years(subcampaigns)
+
+    if isinstance(category, str):
+        category = [category]
+
+    data = [os.path.join(sample_dir, f"obs/{y}/data_0_pseudotop_{c}.root") for c in category for y in years]
+
+    assert(data)
+    if check_exist:
+        for d in data:
+            assert(os.path.isfile(d))
+
+    return data
+
+def get_samples_signal(
+    sample_dir, # top direcotry to look for sample files
+    category = 'ljets', # "ejets" or "mjets" or "ljets"
+    subcampaigns = ["mc16a", "mc16d", "mc16e"],
+    sample_type = 'systCRL', # systCRL or detNP or mcWAlt
+    sample_suffix = 'nominal',
+    check_exist = True, # If True, check if the files exist
+    ):
+
+    if isinstance(category, str):
+        category = [category]
+
+    sample_name = f"{sample_type}/ttbar_{sample_suffix}"
+
+    samples_sig = []
+    for e in subcampaigns:
+        for c in category:
+            s = glob.glob(os.path.join(sample_dir, f"{sample_name}/{e}/ttbar_*_pseudotop_parton_{c}.root"))
+            s.sort()
+            samples_sig += s
+
+    assert(samples_sig)
+    if check_exist:
+        for f in samples_sig:
+            assert(os.path.isfile(f))
+
+    return samples_sig
+
+def get_samples_backgrounds(
+    sample_dir, # top direcotry to look for sample files
+    category = 'ljets', # "ejets" or "mjets" or "ljets"
+    subcampaigns = ["mc16a", "mc16d", "mc16e"],
+    backgrounds = ['fakes', 'Wjets', 'Zjets', 'singleTop', 'ttH', 'ttV', 'VV'],
+    sample_type = 'systCRL', # systCRL or detNP or mcWAlt
+    sample_suffix = 'nominal',
+    check_exist = True, # If True, check if the files exist
+    ):
+
+    if isinstance(category, str):
+        category = [category]
+
+    samples_bkg = []
+
+    for bkg in backgrounds:
+        if bkg.lower() == "fakes":
+            # QCD
+            years = subCampaigns_to_years(subcampaigns)
+            samples_bkg += [os.path.join(sample_dir, f"fakes/{y}/data_0_pseudotop_{c}.root") for c in category for y in years]
+        else:
+            if bkg in ["Wjets", "Zjets"]:
+                sample_name = f"systCRL/{bkg}_nominal" # only one that is available
+            else:
+                sample_name = f"{sample_type}/{bkg}_{sample_suffix}"
+
+            samples_bkg += [os.path.join(sample_dir, f"{sample_name}/{e}/{bkg}_0_pseudotop_{c}.root") for c in category for e in subcampaigns]
+
+    assert(samples_bkg)
+    if check_exist:
+        for f in samples_bkg:
+            assert(os.path.isfile(f))
+
+    return samples_bkg
+
+def write_config_nominal(
+    sample_local_dir,
+    category = "ljets", # "ejets" or "mjets" or "ljets"
+    subcampaigns = ["mc16a", "mc16d", "mc16e"],
+    output_top_dir = '.',
+    outname_config =  'runConfig',
+    common_cfg = {}
+    ):
+
+    # list of samples
+    data_nominal = get_samples_data(sample_local_dir, category, subcampaigns)
+    sig_nominal = get_samples_signal(sample_local_dir, category, subcampaigns)
+    bkg_nominal = get_samples_backgrounds(sample_local_dir, category, subcampaigns)
+
+    # output directory
+    outdir_nominal = os.path.join(output_top_dir, "nominal")
+
+    # config
+    nominal_cfg = common_cfg.copy()
+    nominal_cfg.update({
+        "data": data_nominal,
+        "signal": sig_nominal,
+        "background": bkg_nominal,
+        "outputdir": outdir_nominal,
+        "plot_verbosity": 2
+        })
+
+    # write run configuration to file
+    outname_config_nominal = f"{outname_config}_nominal.json"
+    util.write_dict_to_json(nominal_cfg, outname_config_nominal)
+
+def write_config_bootstrap(
+    sample_local_dir,
+    nresamples,
+    start_index = 0,
+    category = "ljets", # "ejets" or "mjets" or "ljets"
+    subcampaigns = ["mc16a", "mc16d", "mc16e"],
+    output_top_dir = '.',
+    outname_config =  'runConfig',
+    common_cfg = {}
+    ):
+
+    # list of samples
+    data_nominal = get_samples_data(sample_local_dir, category, subcampaigns)
+    sig_nominal = get_samples_signal(sample_local_dir, category, subcampaigns)
+    bkg_nominal = get_samples_backgrounds(sample_local_dir, category, subcampaigns)
+
+    # output directory
+    outdir_bs = os.path.join(output_top_dir, "bootstrap")
+    outdir_bs_dict = {
+        f"resamples{n}": outdir_bs for n in range(start_index, start_index+nresamples)
+    }
+
+    # config
+    bootstrap_cfg = common_cfg.copy()
+    bootstrap_cfg.update({
+        "data": data_nominal,
+        "signal": sig_nominal,
+        "background": bkg_nominal,
+        "outputdir": outdir_bs_dict,
+        "resample_data": True,
+        "run_ibu": False
+        })
+
+    # write run configuration to file
+    outname_config_bootstrap = f"{outname_config}_bootstrap.json"
+    util.write_dict_to_json(bootstrap_cfg, outname_config_bootstrap)
+
+def write_config_systematics(
+    sample_local_dir,
+    systematics_keywords = [],
+    category = "ljets", # "ejets" or "mjets" or "ljets"
+    subcampaigns = ["mc16a", "mc16d", "mc16e"],
+    output_top_dir = '.',
+    outname_config =  'runConfig',
+    common_cfg = {}
+    ):
+
+    cfg_dict_list = []
+
+    # nominal samples:
+    sig_nom = get_samples_signal(sample_local_dir, category, subcampaigns)
+    bkg_nom = get_samples_signal(sample_local_dir, category, subcampaigns)
+
+    # systematics as alternative sets of events in TTrees
+    for syst in get_systematics(systematics_keywords, syst_type="Branch"):
+        print(syst)
+
+        # samples
+        # varied samples as pseudo data
+        sig_syst = get_samples_signal(
+            sample_local_dir, category, subcampaigns,
+            sample_type = 'detNP',
+            sample_suffix = syst
+            )
+
+        # background samples to be mixed with the above signal samples to make pseudo data
+        bkg_syst = get_samples_backgrounds(
+            sample_local_dir, category, subcampaigns,
+            sample_type = 'detNP',
+            sample_suffix = syst
+            )
+
+        # unfold using the nominal samples
+
+        # output directory
+        outdir_syst = os.path.join(output_top_dir, syst)
+
+        syst_cfg = common_cfg.copy()
+        syst_cfg.update({
+            "data": sig_syst,
+            "bdata": bkg_syst,
+            "signal": sig_nom,
+            "background": bkg_nom,
+            "outputdir": outdir_syst,
+            #"load_models": ?
+            #"nruns": ?
+            })
+
+        cfg_dict_list.append(syst_cfg)
+
+    # systematics as scale factor variations
+    for syst, wtype in zip(*get_systematics(systematics_keywords, syst_type="ScaleFactor", get_weight_types=True)):
+        print(syst)
+
+        # output directory
+        outdir_syst = os.path.join(output_top_dir, syst)
+
+        syst_cfg = common_cfg.copy()
+
+        # use nominal samples but different weights as pseudo data
+        syst_cfg.update({
+            "data": sig_nom,
+            "bdata": bkg_nom,
+            "signal": sig_nom,
+            "background": bkg_nom,
+            "weight_data": wtype,
+            "weight_mc": "nominal",
+            "outputdir": outdir_syst,
+            #"load_models": ?
+            #"nruns": ?
+            #"unfolded_weights": ?
+            })
+
+        cfg_dict_list.append(syst_cfg)
+
+    # TODO modelling uncertainties
+
+    print(f"Number of run configs for systematics: {len(cfg_dict_list)}")
+
+    # write run configs to file
+    outname_config_syst = f"{outname_config}_syst.json"
+    util.write_dict_to_json(cfg_dict_list, outname_config_syst)
+
 def getSamples(
     sample_dir, # top directory to read sample files
     category = 'ljets', # "ejets" or "mjets" or "ljets"
@@ -150,101 +402,39 @@ def createRun2Config(
 
     # nominal input files
     print("nominal")
-    obs_nominal, sig_nominal, bkg_nominal = getSamples(
+    write_config_nominal(
         sample_local_dir,
         category = category,
-        systematics = 'nominal',
-        subcampaigns = subcampaigns
-    )
-
-    outdir_nominal = os.path.join(output_top_dir, "nominal")
-
-    nominal_cfg = common_cfg.copy()
-    nominal_cfg.update({
-        "data": obs_nominal,
-        "signal": sig_nominal,
-        "background": bkg_nominal,
-        "outputdir": outdir_nominal,
-        "plot_verbosity": 2,
-        "run_ibu": False
-        })
-
-    # write nominal run configuration
-    outname_config_nominal = f"{outname_config}_nominal.json"
-    util.write_dict_to_json(nominal_cfg, outname_config_nominal)
+        subcampaigns = subcampaigns,
+        output_top_dir = output_top_dir,
+        outname_config = outname_config,
+        common_cfg = common_cfg
+        )
 
     # bootstrap for statistical uncertainties
     if do_bootstrap:
-        nresamples = 10
-        outdir_resample = os.path.join(output_top_dir, "bootstrap")
-        outdir_resample_dict = {
-            f"resample{n}" : outdir_resample for n in range(nresamples)
-            }
-
-        resample_cfg = common_cfg.copy()
-        resample_cfg.update({
-            "data": obs_nominal,
-            "signal": sig_nominal,
-            "background": bkg_nominal,
-            "outputdir": outdir_resample_dict,
-            "resample_data": True
-            })
-
-        # write bootstrap run configuration
-        outname_config_bootstrap = f"{outname_config}_bootstrap.json"
-        util.write_dict_to_json(resample_cfg, outname_config_bootstrap)
+        write_config_bootstrap(
+            sample_local_dir,
+            nresamples = 10,
+            start_index = 0,
+            category = category,
+            subcampaigns = subcampaigns,
+            output_top_dir = output_top_dir,
+            outname_config = outname_config,
+            common_cfg = common_cfg
+            )
 
     # for systematic uncertainties
-    if not do_systematics:
-        return
-
-    cfg_dict_list = []
-
-    # Systematics as separate TTrees
-    for syst in get_systematics(systematics_keywords, syst_type="Branch"):
-        print(syst)
-
-        obs_syst, sig_syst, bkg_syst = getSamples(
+    if do_systematics:
+        write_config_systematics(
             sample_local_dir,
+            systematics_keywords = systematics_keywords,
             category = category,
-            systematics = syst,
-            subcampaigns = subcampaigns
-            )
-
-        outdir_syst = os.path.join(output_top_dir, syst)
-
-        syst_cfg = writeConfig_branch(
-            common_cfg,
-            obs_syst, sig_syst, bkg_syst,
-            outdir = outdir_syst,
-            load_model_dir = outdir_nominal
-            )
-
-        cfg_dict_list.append(syst_cfg)
-
-    # Systematics as scale factor variations
-    for syst, wtype in zip(*get_systematics(systematics_keywords, syst_type="ScaleFactor", get_weight_types=True)):
-        print(syst)
-
-        outdir_syst = os.path.join(output_top_dir, syst)
-
-        syst_cfg = writeConfig_scalefactor(
-            common_cfg,
-            obs_nominal, sig_nominal, bkg_nominal,
-            outdir = outdir_syst,
-            weight_type = wtype,
-            unfolded_weights_dir = outdir_nominal
-            )
-
-        cfg_dict_list.append(syst_cfg)
-
-    # TODO add modelling uncertainties
-
-    print(f"Number of run configs for systematics: {len(cfg_dict_list)}")
-
-    # write systematic run config to file
-    outname_config_syst = f"{outname_config}_syst.json"
-    util.write_dict_to_json(cfg_dict_list, outname_config_syst)
+            subcampaigns = subcampaigns,
+            output_top_dir = output_top_dir,
+            outname_config = outname_config,
+            common_cfg = common_cfg
+        )
 
 if __name__ == "__main__":
 
@@ -284,7 +474,8 @@ if __name__ == "__main__":
         "nruns" : 7,
         "parallel_models" : 3,
         "resample_data" : False,
-        "correct_acceptance" : True
+        "correct_acceptance" : True,
+        "run_ibu": True,
     }
 
     if args.observables:
