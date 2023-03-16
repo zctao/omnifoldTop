cc58b68e51cd201f3880783c7e42f173893ecade
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2023-03-16 15:55:55 -0700

    Update make_run2_config.sh

--- scripts/ttbarDiffXsRun2/make_run2_config.sh
+++ scripts/ttbarDiffXsRun2/make_run2_config.sh
@@ -5,10 +5,8 @@ samples_dir=/mnt/xrootdg/ztao/NtupleTT/latest
 python scripts/ttbarDiffXsRun2/createRun2Config.py \
     -d ${samples_dir} \
     -r ${result_top_dir} \
-    -n configs/run/ttbarDiffXsRun2/runCfg_run2_ljets \
+    -n ${result_top_dir}/configs/runCfg_run2_ljets \
     -e mc16a mc16d mc16e \
     -b \
-    -s all
-    #-s CategoryReduction_JET_Pileup_RhoTopology
-    #-s pileup
-    #-s bTagSF_DL1r_70_eigenvars_B:0
+    -s #-k bTagSF_DL1r_70_eigenvars_B1 CategoryReduction_JET_Pileup_RhoTopology
+    #--observables th_pt
