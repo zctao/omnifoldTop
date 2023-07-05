d4f77d3e8c2d58595c24eae3d83519333f595004
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2023-07-04 17:48:31 -0700

    NNReweighter and script to reweight samples

---
+++ python/nnreweighter.py
@@ -0,0 +1,185 @@
+import numpy as np
+from sklearn.model_selection import KFold
+import hist
+
+from modelUtils import n_models_in_parallel, get_model, get_callbacks, train_model
+import histogramming as myhu
+
+import logging
+
+logger = logging.getLogger("nnreweighter")
+
+def check_weights(w, nevents=None):
+    if w is None:
+        if nevents is None:
+            raise RuntimeError(f"Number of events is not specified")
+        else:
+            w = [np.ones(nevents)] * n_models_in_parallel
+    elif not isinstance(w, list):
+        w = [w] * n_models_in_parallel
+    elif len(w) != n_models_in_parallel:
+        raise RuntimeError(f"The number of weight arrays provided in the list ({len(w)}) is inconsistent with the number of parallel models ({n_models_in_parallel})")
+
+    return w
+
+def set_up_model(
+    model_type, # str, type of the network
+    input_shape,
+    model_filepath_load = None, # str, filepath to load trained model weights
+    resume_training = False, # bool
+    ):
+
+    model = get_model(input_shape=input_shape, nclass=2, model_type=model_type)
+
+    # load model
+    if model_filepath_load:
+        try:
+            model.load_weights(model_filepath_load)
+            logger.info(f"Load model from {model_filepath_load}")
+        except:
+            if resume_training:
+                logger.debug(f"Cannot load model from {model_filepath_load}. Continue to train models from here.")
+            else:
+                logger.critical(f"Cannot load model from {model_filepath_load}")
+                raise RuntimeError("Model loading failure")
+
+    return model
+
+def predict(model, events, batch_size):
+    events_list = [events for _ in range(n_models_in_parallel)]
+    preds = model.predict(events_list, batch_size=batch_size)
+    preds = np.squeeze(preds)
+
+    if n_models_in_parallel == 1:
+        preds = np.reshape(preds, (n_models_in_parallel,)+np.shape(preds)) # happens after squeezing, so that we keep the first dimension
+
+    return preds
+
+def reweight(preds):
+    return np.nan_to_num( preds / (1. - preds))
+
+def train_and_reweight(
+    # Inputs
+    X_target, 
+    X_source,
+    w_target = None,
+    w_source = None,
+    # model
+    model_type = 'dense_100x3',
+    skip_training = False,
+    model_filepath_load = None,
+    model_filepath_save = None,
+    # Training
+    resume_training = False,
+    nsplit_cv = 2,
+    batch_size = 20000,
+    epochs = 100,
+    # Reweight
+    calibrate=False,
+    # Logging
+    verbose= False
+    ):
+
+    if verbose:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    # event weights
+    w_target = check_weights(w_target, nevents = len(X_target))
+    w_source = check_weights(w_source, nevents = len(X_source))
+
+    # labels
+    Y_target = np.ones(len(X_target))
+    Y_source = np.zeros(len(X_source))
+
+    # model outputs
+    preds_source = np.empty(shape=(n_models_in_parallel, len(X_source)))
+
+    # calibrator
+    if calibrate:
+        nbins = 10 + int(len(X_source) ** (1. / 3.))
+        logger.debug(f"calibrator nbins = {nbins}")
+        histogram_1 = [ hist.Hist(hist.axis.Regular(nbins, 0., 1.), storage=hist.storage.Weight()) for _ in range(n_models_in_parallel) ]
+        histogram_0 = [ hist.Hist(hist.axis.Regular(nbins, 0., 1.), storage=hist.storage.Weight()) for _ in range(n_models_in_parallel) ]
+
+    # K-fold cross validation
+    logger.debug(f"nsplit_cv = {nsplit_cv}")
+
+    if nsplit_cv > 1:
+        if model_filepath_load:
+            model_filepath_load = model_filepath_load + "_cv{}"
+        if model_filepath_save:
+            model_filepath_save = model_filepath_save + "_cv{}"
+
+    kf = KFold(nsplit_cv)
+
+    for icv, ((index_train_source, index_test_source), (index_train_target, index_test_target)) in enumerate(zip(kf.split(X_source), kf.split(X_target))):
+        logger.info(f"KFold: {icv}")
+
+        model_filepath_load_icv = model_filepath_load.format(icv) if model_filepath_load else None
+        model_filepath_save_icv = model_filepath_save.format(icv) if model_filepath_save else None
+
+        # set up network
+        classifier = set_up_model(
+            model_type, 
+            input_shape = X_source.shape[1:],
+            model_filepath_load = model_filepath_load_icv,
+            resume_training = resume_training
+            )
+
+        if skip_training:
+            logger.info("Use trained model for reweighting")
+
+        else:
+            logger.info("Start training")
+
+            X_train = np.concatenate([X_source[index_train_source], X_target[index_train_target]])
+            Y_train = np.concatenate([Y_source[index_train_source], Y_target[index_train_target]])
+            w_train = np.concatenate([np.asarray(w_source)[:,index_train_source], np.asarray(w_target)[:,index_train_target]], axis=1)
+
+            train_model(
+                classifier,
+                X_train, Y_train, w_train,
+                batch_size = batch_size, epochs = epochs,
+                callbacks = get_callbacks(model_filepath_save_icv),
+                )
+            
+            logger.info("Training done")
+
+            if model_filepath_save_icv:
+                logger.info("Save model to " + model_filepath_save_icv)
+                classifier.save_weights(model_filepath_save_icv)
+
+        # reweight
+        logger.info("Reweight")
+
+        preds_source[:,index_test_source] = predict(
+            classifier, X_source[index_test_source], batch_size=batch_size
+            )
+
+        if calibrate:
+            preds_test_target = predict(
+                classifier, X_target[index_test_target], batch_size=batch_size
+                )
+
+            for i in range(n_models_in_parallel):
+                histogram_1[i].fill(
+                    preds_test_target[i], weight=w_target[i][index_test_target]
+                    )
+
+                histogram_0[i].fill(
+                    preds_source[i,index_test_source], weight=w_source[i][index_test_source]
+                    )
+
+    # end of cross validation loop
+
+    if calibrate:
+        rw = np.empty_like(preds_source)
+        for i in range(n_models_in_parallel):
+            histogram_r = myhu.divide(histogram_1[i], histogram_0[i])
+            rw[i] = myhu.read_histogram_at_locations(preds_source[i], histogram_r)
+        return rw
+    else:
+        # direct reweighting
+        return reweight(preds_source)
---
+++ scripts/reweight_samples.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python3
+import os
+import sys
+import argparse
+import logging
+import numpy as np
+import h5py
+
+import util
+import modelUtils
+from OmniFoldTTbar import getDataHandler
+from nnreweighter import train_and_reweight
+import plotter
+
+def reweight_samples(**parsed_args):
+
+    # Prepare logging
+    util.configRootLogger()
+    logger = logging.getLogger("nnreweight")
+    logger.setLevel(logging.DEBUG if parsed_args['verbose']>0 else logging.INFO)
+
+    logger.info(f"Hostname: {os.uname().nodename}")
+
+    if not os.path.isdir(parsed_args['outputdir']):
+        os.makedirs(parsed_args['outputdir'])
+
+    # Print arguments to logger
+    for argkey, argvalue in sorted(parsed_args.items()):
+        if argvalue is None:
+            continue
+        logger.info(f"Argument {argkey}: {argvalue}")
+
+    # store arguments to json file
+    argname = 'arguments.json'
+    fname_args = os.path.join(parsed_args['outputdir'], argname)
+    logger.info(f"Write arguments to file {fname_args}")
+    util.write_dict_to_json(parsed_args, fname_args)
+
+    ######
+    # variables
+    obsCfg_d = util.read_dict_from_json(parsed_args['observable_config'])
+    # FIXME hardcode 'branch_det' for reco-level variables only for now
+    varnames = [ obsCfg_d[key]['branch_det'] for key in parsed_args['observables'] ]
+
+    ######
+    # Load input data files
+    logger.info(f"Load target samples: {' '.join(parsed_args['target'])}")
+    dh_target = getDataHandler(parsed_args['target'], varnames)
+    logger.info(f"Total number of events in target: {len(dh_target)}")
+
+    logger.info(f"Load source samples: {' '.join(parsed_args['source'])}")
+    dh_source = getDataHandler(parsed_args['source'], varnames)
+    logger.info(f"Total number of events in source: {len(dh_source)}")
+
+    ######
+    # feature arrays
+    logger.info("Cconstruct feature and label arrays")
+    target_arr = dh_target[varnames]
+    source_arr = dh_source[varnames]
+
+    # preprocessing
+    X = np.concatenate([target_arr, source_arr])
+    xmean = np.mean(X, axis=0)
+    xstd = np.mean(X, axis=0)
+
+    target_arr -= xmean
+    target_arr /= xstd
+
+    source_arr -= xmean
+    source_arr /= xstd
+
+    # event weights
+    logger.info("Prepare weights")
+    w_target = dh_target.get_weights()
+
+    if parsed_args['normalize_weight']:
+        logger.info(f"Rescale source weights to be consistant with the target weights")
+        sumw_target = dh_target.sum_weights()
+        sumw_source = dh_source.sum_weights()
+        dh_source.rescale_weights(sumw_target/sumw_source)
+
+    w_source = dh_source.get_weights()
+
+    logger.debug(f"total weights (target): {dh_target.sum_weights()}")
+    logger.debug(f"total weights (source): {dh_source.sum_weights()}")
+
+    ##
+    # Plot input distributions
+    if parsed_args['plot_verbosity'] > 1:
+        pass
+
+    if parsed_args['weights_file']:
+        file_rw = h5py.File(parsed_args['weights_file'], 'r')
+        rw = file_rw['rw']
+    else:
+        modelUtils.n_models_in_parallel = 1
+
+        model_dir = os.path.join(parsed_args['outputdir'],"Models")
+        if not os.path.isdir(model_dir):
+            logger.debug(f"Create directory {model_dir}")
+            os.makedirs(model_dir)
+
+        file_rw = h5py.File(os.path.join(parsed_args['outputdir'], "reweights.h5"), "w")
+
+        rw = file_rw.create_dataset("rw", shape=(len(source_arr),))
+        rw[:] = train_and_reweight(
+            X_target = target_arr,
+            X_source = source_arr,
+            w_target = w_target,
+            w_source = w_source,
+            model_type = parsed_args["model_type"],
+            model_filepath_save = os.path.join(model_dir, "model"),
+            nsplit_cv = 2,
+            batch_size = parsed_args['batch_size'],
+            epochs = 100,
+            calibrate = parsed_args['reweight_method']=='histogram',
+            verbose= parsed_args['verbose']
+        )[0]
+        # [0] because modelUtils.n_models_in_parallel = 1
+
+    if parsed_args['plot_verbosity'] > 0:
+        # plot distributions of the weights
+        logger.info("Plot the weight distribution")
+        plotter.plot_LR_distr(
+            os.path.join(parsed_args['outputdir'], "rw_distr"),
+            [rw[:]],
+            nbins = 10 + int(len(w_source) ** (1. / 3.))
+            )
+
+        # binning config
+        binCfg_d = util.get_bins_dict(parsed_args['binning_config'])
+
+        target_style = {'color':'black', 'label':'Target', 'histtype':'step'}
+        source_style = {'color':'blue', 'label':'Source', 'histtype':'step'}
+        reweighted_style = {'color':'tab:red', 'label':'Reweighted', 'histtype':'step'}
+
+        # compare reweighted source with target
+        for obs, vname in zip(parsed_args['observables'], varnames):
+            logger.info(f"Plot observable {obs}")
+
+            binedges = binCfg_d[obs]
+            absV = '_abs' in obs
+
+            hist_target = dh_target.get_histogram(vname, binedges, absoluteValue=absV)
+            hist_source = dh_source.get_histogram(vname, binedges, absoluteValue=absV)
+            hist_source_rw = dh_source.get_histogram(vname, binedges, weights=rw[:], absoluteValue=absV)
+
+            plotter.plot_histograms_and_ratios(
+                figname = os.path.join(parsed_args['outputdir'], obs),
+                hists_numerator = [hist_source_rw, hist_source],
+                hist_denominator = hist_target,
+                draw_options_numerator = [reweighted_style, source_style],
+                draw_option_denominator = target_style,
+                xlabel = obsCfg_d[obs]['xlabel'],
+                ylabel = obsCfg_d[obs]['ylabel'],
+                ylabel_ratio = "Ratio to Target"
+            )
+
+    file_rw.close()
+
+def getArgsParser(arguments_list=None, print_help=False):
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-t', '--target', required=True, nargs='+', type=str,
+                        help="Target sample to reweight to as a list of root files")
+    parser.add_argument('-s', '--source', required=True, nargs='+', type=str,
+                        help="Source sample to be reweighted")
+    parser.add_argument('--observables', nargs='+', type=str,
+                        default=['th_pt','th_y','tl_pt','tl_y','mtt','ptt','ytt'],
+                        help="List of observables used to train the classifier")
+    parser.add_argument('--observable-config',
+                        default='configs/observables/vars_ttbardiffXs_pseudotop.json',
+                        help="JSON configurations for observables")
+    parser.add_argument('-o', '--outputdir', type=str, default='.',
+                        help="Output directory")
+    parser.add_argument('-n', '--normalize-weight', action='store_true',
+                        help="If True, renormalize source weights to target weights")
+    parser.add_argument('-m', '--model-type', type=str, default='dense_100x3',
+                        help="Type of NN")
+    parser.add_argument('--batch-size', type=int, default=20000,
+                        help="Training batch size")
+    parser.add_argument('-v', '--verbose', action='store_true', 
+                        help="If True, set logging level to DEBUG else INFO")
+    parser.add_argument('-r', '--reweight-method', choices=['direct','histogram'],
+                        default='histogram', help="Method to reweight source to target")
+    parser.add_argument('-p', '--plot-verbosity', action='count', default=0,
+                        help="Plot verbosity level")
+    parser.add_argument('-w', '--weights-file', type=str,
+                        help="File path to the weights file. If provided, load weights from this file and skip training")
+    parser.add_argument("--binning-config", type=str,
+                        default='configs/binning/bins_ttdiffxs.json',
+                        help="Path to the binning config file for variables.")
+
+    if print_help:
+        parser.print_help()
+
+    args = parser.parse_args(arguments_list)
+
+    return args
+
+if __name__ == "__main__":
+
+    try:
+        args = getArgsParser()
+    except Exception as e:
+        sys.exit(f"Config Failure: {e}")
+
+    # unfold
+    reweight_samples(**vars(args))
