5a5645f218e4730026e906fa6d080f878a111c48
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2024-02-21 11:45:20 -0800

    Update scripts

--- test/ttbarDiffXsRun2/run_uncertainties_eval.sh
+++ test/ttbarDiffXsRun2/run_uncertainties_eval.sh
@@ -10,28 +10,32 @@ groups="JES BTag Lepton MET Pileup IFSR MTop hdamp Hadronization Generator Backg
 #common_args="${fpath_central} -s ${fpath_syst} -t ${fpath_network} -p -v -g ${groups} --observables th_pt th_y tl_pt tl_y mtt ptt ytt ytt_abs th_y_abs"
 common_args="${fpath_central} -s ${fpath_syst} -t ${fpath_network} -p -v -g ${groups}"
 
-# absolute
-python scripts/ttbarDiffXsRun2/evaluate_uncertainties.py ${common_args} -o ${output_topdir}/abs/
+run_eval() {
+    dname="$1"
+    shift
+    extra_args="$@"
 
+    python scripts/ttbarDiffXsRun2/evaluate_uncertainties.py ${common_args} -o ${output_topdir}/${dname}/ ${extra_args}
+}
+
+# absolute
+run_eval abs
 # relative
-python scripts/ttbarDiffXsRun2/evaluate_uncertainties.py ${common_args} -o ${output_topdir}/rel/ --normalize
+run_eval rel --normalize
 
 ### symmetrize
 # absolute
-python scripts/ttbarDiffXsRun2/evaluate_uncertainties.py ${common_args} -o ${output_topdir}/abs_sym/ --symmetrize
-
+run_eval abs_sym --symmetrize
 # relative
-python scripts/ttbarDiffXsRun2/evaluate_uncertainties.py ${common_args} -o ${output_topdir}/rel_sym/ --normalize --symmetrize
+run_eval rel_sym --normalize --symmetrize
 
 ### trim
-python scripts/ttbarDiffXsRun2/evaluate_uncertainties.py ${common_args} -o ${output_topdir}/abs_trim_p005/ --trim-threshold 0.005
-
+run_eval abs_trim_p005 --trim-threshold 0.005
 # relative
-python scripts/ttbarDiffXsRun2/evaluate_uncertainties.py ${common_args} -o ${output_topdir}/rel_trim_p001/ --normalize --trim-threshold 0.001
+run_eval rel_trim_p001 --normalize --trim-threshold 0.001
 
 ### trim and symmetrize
 # absolute
-python scripts/ttbarDiffXsRun2/evaluate_uncertainties.py ${common_args} -o ${output_topdir}/abs_sym_trim_p005/ --symmetrize --trim-threshold 0.005
-
+run_eval abs_sym_trim_p005 --symmetrize --trim-threshold 0.005
 # relative
-python scripts/ttbarDiffXsRun2/evaluate_uncertainties.py ${common_args} -o ${output_topdir}/rel_sym_trim_p001/ --normalize --symmetrize --trim-threshold 0.001
+run_eval rel_sym_trim_p001 --normalize --symmetrize --trim-threshold 0.001
