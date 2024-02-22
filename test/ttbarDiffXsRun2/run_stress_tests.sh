87f6276efda9d62e4f0dcd90fb536864edca0296
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2024-02-21 20:49:25 -0800

    Remake histograms for stress tests

--- test/ttbarDiffXsRun2/run_stress_tests.sh
+++ test/ttbarDiffXsRun2/run_stress_tests.sh
@@ -7,6 +7,7 @@ outdir_rw=${DATA_DIR}/OmniFoldOutputs/Run2TTbarXs/Reweight/$timestamp
 outdir_test=${DATA_DIR}/OmniFoldOutputs/Run2TTbarXs/StressTests/$timestamp
 
 observables='mtt ptt th_pt tl_pt ytt th_y tl_y'
+observables_multidim='ptt_vs_mtt th_pt_vs_mtt ptt_vs_ytt_abs mtt_vs_ytt_abs mtt_vs_ptt_vs_ytt_abs mtt_vs_th_pt_vs_th_y_abs mtt_vs_th_pt_vs_ytt_abs mtt_vs_th_y_abs_vs_ytt_abs'
 
 #######
 echo "Generate pseudo data by reweighting signal MC to data"
@@ -35,4 +36,16 @@ cfg_suffix=(stress_data stress_data_alt nominal stress_th_pt stress_bump)
 for sfx in ${cfg_suffix[@]}; do
     echo $sfx
     python ${SOURCE_DIR}/run_unfold.py ${outdir_test}/configs/runCfg_${sfx}.json
+done
+
+######
+# re-make histograms with more observables (2D and 3D)
+echo "Remake histograms"
+for sfx in ${cfg_suffix[@]}; do
+    echo $sfx
+    python ${SOURCE_DIR}/scripts/make_histograms.py ${outdir_test}/${sfx} \
+        --binning-config ${SOURCE_DIR}/configs/binning/bins_ttdiffxs.json \
+        --observables $observables \
+        --observables-multidim ${observables_multidim} \
+        --include-ibu -v
 done
