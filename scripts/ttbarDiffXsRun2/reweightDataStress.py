0866c16cd25ad961f9c2961f447d4b4d5b576e41
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2025-03-21 16:25:31 -0700

    Add back background samples for stress tests

--- scripts/ttbarDiffXsRun2/reweightDataStress.py
+++ scripts/ttbarDiffXsRun2/reweightDataStress.py
@@ -10,7 +10,7 @@ def reweightDataStress(
 
     data = r2c.get_samples_data(sample_dir, subcampaigns=subcampaigns)
     signal = r2c.get_samples_signal(sample_dir, subcampaigns=subcampaigns)
-    #backgrounds = r2c.get_samples_backgrounds(sample_dir, subcampaigns=subcampaigns)
+    backgrounds = r2c.get_samples_backgrounds(sample_dir, subcampaigns=subcampaigns)
 
     args_list = ['-t']+data + ['-s']+signal + ['-b']+backgrounds
     args_list += ['--observables'] + observables
