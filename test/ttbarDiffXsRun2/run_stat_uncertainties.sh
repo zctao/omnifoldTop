a1f6c7c46497128525fd33327ff541740e4122ce
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2025-06-10 15:50:09 -0700

    Update stat uncertainty script

--- test/ttbarDiffXsRun2/run_stat_uncertainties.sh
+++ test/ttbarDiffXsRun2/run_stat_uncertainties.sh
@@ -10,24 +10,25 @@ observables_multidim='ptt_vs_mtt th_pt_vs_mtt ytt_abs_vs_mtt ptt_vs_ytt_abs mtt_
 
 ######
 # Generate run configs for bootstraping
-python ${SOURCE_DIR}/scripts/ttbarDiffXsRun2/createRun2Config.py \
+echo python ${SOURCE_DIR}/scripts/ttbarDiffXsRun2/createRun2Config.py \
     --sample-dir ${sample_dir} \
     --result-dir ${outdir} \
     --config-name ${outdir}/configs/runCfg \
     --subcampaigns $subcampaigns \
     --observables $observables \
-    --run-list bootstrap
+    --run-list bootstrap \
+    -b ubc
 
 ######
 # Run unfolding
-python ${SOURCE_DIR}/run_unfold.py ${outdir}/configs/runCfg_bootstrap.json
-python ${SOURCE_DIR}/run_unfold.py ${outdir}/configs/runCfg_bootstrap_mc.json
+echo python ${SOURCE_DIR}/run_unfold.py ${outdir}/configs/runCfg_bootstrap.json
+echo python ${SOURCE_DIR}/run_unfold.py ${outdir}/configs/runCfg_bootstrap_mc.json
 
 ######
 # Make histograms
 bs_dir=${outdir}/bootstrap
 for i in {0..9}; do
-    python ${SOURCE_DIR}/scripts/make_histogramsv2.py ${bs_dir}/resamples${i} \
+    echo python ${SOURCE_DIR}/scripts/make_histogramsv2.py ${bs_dir}/resamples${i} \
         --binning-config ${SOURCE_DIR}/configs/binning/bins_ttdiffxs.json \
         --observables ${observables} ${observables_multidim} \
         --include-ibu \
@@ -36,7 +37,7 @@ done
 
 bs_mc_dir=${outdir}/bootstrap_mc
 for i in {0..9}; do
-    python ${SOURCE_DIR}/scripts/make_histogramsv2.py ${bs_mc_dir}/resamples${i} \
+    echo python ${SOURCE_DIR}/scripts/make_histogramsv2.py ${bs_mc_dir}/resamples${i} \
         --binning-config ${SOURCE_DIR}/configs/binning/bins_ttdiffxs.json \
         --observables ${observables} ${observables_multidim} \
         --include-ibu \
