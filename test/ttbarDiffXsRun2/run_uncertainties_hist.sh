4c380385ff7d7cd1b2a5ff9209180713c3998af1
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2025-06-10 15:50:47 -0700

    Add jobs

--- test/ttbarDiffXsRun2/run_uncertainties_hist.sh
+++ test/ttbarDiffXsRun2/run_uncertainties_hist.sh
@@ -32,3 +32,39 @@ run_hist_keywords() {
 #run_hist_group hist_btag BTag
 #or:
 #run_hist_keywords hist_btag_extrap bTagSF_DL1r_70_extrapolation
+#run_hist_keywords hist_btag_eigenB bTagSF_DL1r_70_eigenvars_B
+#run_hist_keywords hist_btag_eigenC bTagSF_DL1r_70_eigenvars_C
+#run_hist_keywords hist_btag_eigenL bTagSF_DL1r_70_eigenvars_Light
+#run_hist_keywords hist_theo_scale scale_mu isr_ fsr_
+#run_hist_keywords hist_pdf_1 PDF4LHC15_1
+#run_hist_keywords hist_pdf_2 PDF4LHC15_2
+#run_hist_keywords hist_pdf_3 PDF4LHC15_3 PDF4LHC15_4 PDF4LHC15_5 PDF4LHC15_6 PDF4LHC15_7 PDF4LHC15_8 PDF4LHC15_9
+#run_hist_keywords hist_sigmodel hdamp mtop_mt matching_pthard1 ps_hw recoil lineshape_madspin
+#run_hist_keywords hist_jet_det JET_EffectiveNP_Detector
+#run_hist_keywords hist_jet_mix JET_EffectiveNP_Mixed
+#run_hist_keywords hist_jet_mod JET_EffectiveNP_Modelling
+#run_hist_keywords hist_jet_stat JET_EffectiveNP_Statistical
+#run_hist_keywords hist_jet_eta JET_EtaIntercalibration
+#run_hist_keywords hist_jet_flavor JET_Flavor
+#run_hist_keywords hist_jet_flav_perjet JET_Flavour_PerJet
+#run_hist_keywords hist_jet_jer_datavsmc JET_JER_DataVsMC_MC16
+#run_hist_keywords hist_jer_eff_1 JET_JER_EffectiveNP_1_ JET_JER_EffectiveNP_2_ JET_JER_EffectiveNP_3_
+#run_hist_keywords hist_jer_eff_2 JET_JER_EffectiveNP_4_ JET_JER_EffectiveNP_5_ JET_JER_EffectiveNP_6_
+#run_hist_keywords hist_jer_eff_3 JET_JER_EffectiveNP_7_ JET_JER_EffectiveNP_8_ JET_JER_EffectiveNP_9_
+#run_hist_keywords hist_jer_eff_4 JET_JER_EffectiveNP_10_ JET_JER_EffectiveNP_11_ JET_JER_EffectiveNP_12
+#run_hist_keywords hist_jet_pileup JET_Pileup
+#run_hist_keywords hist_jet_others JET_PunchThrough_MC16 JET_SingleParticle_HighPt
+
+#run_hist_keywords hist_eg EG_RESOLUTION EG_SCALE
+#run_hist_keywords hist_met MET_SoftTrk
+#run_hist_keywords hist_muon MUON
+#run_hist_keywords hist_pu_jvt pileup_DOWN pileup_UP jvt_DOWN jvt_UP
+
+#run_hist_keywords hist_lepsf_el leptonSF_EL_SF
+#run_hist_keywords hist_lepsf_mu1 leptonSF_MU_SF_Trigger leptonSF_MU_SF_Isol eptonSF_MU_SF_TTVA
+#run_hist_keywords hist_lepsf_mu2 leptonSF_MU_SF_ID
+
+#run_hist_keywords hist_ifsr isr_ fsr_
+
+#run_hist_keywords hist_bkg singleTop_ ttV_ Wjets_ Zjets_ fakes_ VV_
+#run_hist_keywords hist_lumi lumi_
